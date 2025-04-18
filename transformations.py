a5804157536dd3ec92c049ea007f35739aaed289
d4min <danishamin.rb@gmail.com>
2025-04-19 00:46:09 +0100

    refactoring date and category conversion to return self.df instead of a series, also makes date conversion simpler

--- transformations.py
+++ transformations.py
@@ -13,13 +13,15 @@ class DataTransformer:
         """
         convert a column to dtype category
         """
-        return self.df[column_name].astype('category')
+        self.df[column_name] = self.df[column_name].astype('category')
+        return self.df
 
     def convert_to_datetime(self, column_name):
         """
         convert a column to datetime
         """
-        self.df[column_name] = pd.to_datetime(self.df[column_name], format='mixed')
-        self.df[column_name] = self.df[column_name].astype('datetime64[ns]')
-        return self.df[column_name].dt.strftime()
+        self.df[column_name] = pd.to_datetime(self.df[column_name], format='%b-%Y')
+        return self.df
+    
+
     
